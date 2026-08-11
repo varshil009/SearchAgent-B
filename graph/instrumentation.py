2c19befb88725abf59f5260b0d4a5ba7134ce28c
Varshil Prajapati <118505855+varshil009@users.noreply.github.com>
2026-08-11 19:44:09 +0530

    commented out phoenix tracing for now

--- graph/instrumentation.py
+++ graph/instrumentation.py
@@ -1,11 +1,15 @@
+"""
 try:
     import phoenix as px
+    import os
     from phoenix.otel import register
 except ImportError:
     tracer_provider = None
 else:
-    px.launch_app()
+    if os.getenv("USE_PHOENIX") is None:
+        px.launch_app()
     tracer_provider = register(
         project_name="local-search-chatbot",
         auto_instrument=True
     )
+"""
